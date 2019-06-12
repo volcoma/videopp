7c8093c8fa98acd7551897e9747b803beb5044ca
Nikolay Ivanov <nikolay.ivanov@egt-bg.com>
2019-06-12 15:30:20 +0300

    fixed wrong check

--- videopp/text.cpp
+++ videopp/text.cpp
@@ -1,5 +1,6 @@
 #include "text.h"
 #include "font.h"
+#include <fontpp/font.h>
 #include <algorithm>
 #include <iostream>
 
@@ -138,83 +139,6 @@ std::pair<float, float> get_alignment_offsets(text::alignment alignment,
     return {align_to_pixel(xoffs), align_to_pixel(yoffs)};
 }
 
-// Convert UTF-8 to 32-bits character, process single character input.
-// Based on stb_from_utf8() from github.com/nothings/stb/
-// We handle UTF-8 decoding error by skipping forward.
-// Returns how many chars have been read from the in_text
-int unicode_char_from_utf8(uint32_t* out_char, const char* in_text, const char* in_text_end)
-{
-    uint32_t c = uint32_t(-1);
-    const unsigned char* str = (const unsigned char*)in_text;
-    if(!(*str & 0x80))
-    {
-        c = (unsigned int)(*str++);
-        *out_char = c;
-        return 1;
-    }
-    if((*str & 0xe0) == 0xc0)
-    {
-        *out_char = 0xFFFD; // will be invalid but not end of string
-        if(in_text_end && in_text_end - (const char*)str < 2)
-            return 0;
-        if(*str < 0xc2)
-            return 2;
-        c = (unsigned int)((*str++ & 0x1f) << 6);
-        if((*str & 0xc0) != 0x80)
-            return 2;
-        c += (*str++ & 0x3f);
-        *out_char = c;
-        return 2;
-    }
-    if((*str & 0xf0) == 0xe0)
-    {
-        *out_char = 0xFFFD; // will be invalid but not end of string
-        if(in_text_end && in_text_end - (const char*)str < 3)
-            return 0;
-        if(*str == 0xe0 && (str[1] < 0xa0 || str[1] > 0xbf))
-            return 3;
-        if(*str == 0xed && str[1] > 0x9f)
-            return 3; // str[1] < 0x80 is checked below
-        c = (unsigned int)((*str++ & 0x0f) << 12);
-        if((*str & 0xc0) != 0x80)
-            return 3;
-        c += (unsigned int)((*str++ & 0x3f) << 6);
-        if((*str & 0xc0) != 0x80)
-            return 3;
-        c += (*str++ & 0x3f);
-        *out_char = c;
-        return 3;
-    }
-    if((*str & 0xf8) == 0xf0)
-    {
-        *out_char = 0xFFFD; // will be invalid but not end of string
-        if(in_text_end && in_text_end - (const char*)str < 4)
-            return 0;
-        if(*str > 0xf4)
-            return 4;
-        if(*str == 0xf0 && (str[1] < 0x90 || str[1] > 0xbf))
-            return 4;
-        if(*str == 0xf4 && str[1] > 0x8f)
-            return 4; // str[1] < 0x80 is checked below
-        c = (unsigned int)((*str++ & 0x07) << 18);
-        if((*str & 0xc0) != 0x80)
-            return 4;
-        c += (unsigned int)((*str++ & 0x3f) << 12);
-        if((*str & 0xc0) != 0x80)
-            return 4;
-        c += (unsigned int)((*str++ & 0x3f) << 6);
-        if((*str & 0xc0) != 0x80)
-            return 4;
-        c += (*str++ & 0x3f);
-        // utf-8 encodings of values used in surrogate pairs are invalid
-        if((c & 0xFFFFF800) == 0xD800)
-            return 4;
-        *out_char = c;
-        return 4;
-    }
-    *out_char = 0;
-    return 0;
-}
 }
 
 text::text() = default;
@@ -512,7 +436,7 @@ void text::regen_unicode_text()
     {
         uint32_t uchar;
 
-        int m = unicode_char_from_utf8(&uchar, beg, end);
+        int m = fnt::text_char_from_utf8(&uchar, beg, end);
         if(!m)
             break;
         unicode_text_.push_back(uchar);
@@ -527,6 +451,12 @@ void text::update_geometry() const
     {
         return;
     }
+
+    if(unicode_text_.empty())
+    {
+        return;
+    }
+
     if(!geometry_.empty())
     {
         return;
